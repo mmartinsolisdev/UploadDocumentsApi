4e5fa5b698f2f1a5ecdca63bbf267b8327ad27fb
Mguel Martin <mmartinsolis.dev@gmail.com>
2021-04-14 16:22:04 -0500

    Fiber CORS Configuration and fiber recover for error handling

--- main.go
+++ main.go
@@ -5,17 +5,26 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 
 	//Se inicializa la app con fiber
 	app := fiber.New()
-
-  app.Get("/", func(c *fiber.Ctx) error {
-  return c.SendString("Hello, World!")
-})
-
+	//Se inicializa cors de fiber para habilitar Cross-Origin Resource Sharing
+	app.Use(cors.New(cors.Config{
+		Next:             nil,
+		AllowOrigins:     "*",
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders:  		"Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, application/json",
+		AllowCredentials: false,
+		ExposeHeaders:    "",
+		MaxAge:           0,
+}))
+//Se inicializa recover de fiber para el manejo de errores
+	app.Use(recover.New())
 	log.Println("Server will start at http://localhost:3000/")
 	log.Fatal(app.Listen(":3000"))
 }
